1dc047ba08c8e3207c9e303ea57eaaa3e4353097
love4echo <sophia1irene@gmail.com>
2025-03-18 23:23:37 -0500

    idk but I made it pink

--- vit5/my-app/src/App.js
+++ vit5/my-app/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  // Use BrowserRouter as Router
+import Navbar from './components/Navbar';  // Import Navbar
 import './App.css';
 
-function App() {
+const Home = () => {
+  return <h2>Home Page</h2>;
+};
+
+const NotHome = () => {
+  return <h2>NotHome Page</h2>;
+};
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        
+        <Routes>
+          <Route path="/Home" element={<Home />} />
+          <Route path="/NotHome" element={<NotHome />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ vit5/my-app/src/pages/NotHome/NotHome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotHome = () => {
+  return (
+    <div>
+      <h2>NotHome Page</h2>
+      <p>hiii</p>
+    </div>
+  );
+};
+
+export default NotHome;
